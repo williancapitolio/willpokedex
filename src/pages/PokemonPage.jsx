69c3ece272997c21e5aa29ad0a89eca9713dbe15
Willian <williansilvap25@gmail.com>
2023-02-10 00:22:42 -0300

    pokemons page ok

--- src/pages/PokemonPage.jsx
+++ src/pages/PokemonPage.jsx
@@ -3,21 +3,22 @@ import { useState, useEffect } from "react";
 import { api } from "../services/Api";
 import { Loader } from "../components/Loader";
 import { Row, Col, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const PokemonPage = () => {
     const { id } = useParams();
     const [pokemonDetails, setPokemonDetails] = useState();
     const [loading, setLoading] = useState(true);
 
+    const getPokemonData = async (id) => {
+        const res = await api.get(`pokemon/${id}`);
+        return res;
+    };
+
     const getPokemon = async (id) => {
         const details = await getPokemonData(id);
         setPokemonDetails(details.data);
         setLoading(false);
-    };
-
-    const getPokemonData = async (id) => {
-        const res = await api.get(`pokemon/${id}`);
-        return res;
     };
 
     useEffect(() => {
@@ -43,13 +44,132 @@ export const PokemonPage = () => {
                                 { border: "none" }
                             }
                         >
-                            <Card.Img
-                                style={
-                                    { width: "15rem" }
-                                }
-                                src={pokemonDetails.sprites.front_default}
-                                variant="top"
-                            />
+                            <Link
+                                to={`/pokemon/${pokemonDetails.id}`}
+                            >
+                                <Card.Img
+                                    style={
+                                        { width: "15rem" }
+                                    }
+                                    src={pokemonDetails.sprites.front_default}
+                                    variant="top"
+                                />
+                            </Link>
+                            <Card.Body
+                                className={`${pokemonDetails.types[0].type.name} rounded text-white`}
+                            >
+                                <Link
+                                    to={`/pokemon/${pokemonDetails.id}`}
+                                    className="link-name"
+                                >
+                                    <Card.Title
+                                        as="div"
+                                    >
+                                        <strong>
+                                            #{pokemonDetails.id} - {pokemonDetails.name.charAt(0).toUpperCase() + pokemonDetails.name.slice(1)}
+                                        </strong>
+                                    </Card.Title>
+                                </Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col
+                        xs={12}
+                        sm={12}
+                        md={12}
+                        lg={12}
+                        xl={12}
+                    >
+                        <Card
+                            className="p-3 rounded text-center shadow p-3 mb-5 bg-white"
+                            style={
+                                { border: "none" }
+                            }
+                        >
+                            <Card.Body>
+                                <Card.Text>
+                                    <Row>
+                                        {pokemonDetails.types.map(t => (
+                                            <Col
+                                                key={t.type.name}
+                                            >
+                                                <div
+                                                    className={`${t.type.name} rounded px-4 py-1`}
+                                                    style={
+                                                        { color: "#FFF" }
+                                                    }
+                                                >
+                                                    {t.type.name.toUpperCase()}
+                                                </div>
+                                            </Col>
+                                        ))}
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                            <Card.Img
+                                                style={
+                                                    { width: "15rem" }
+                                                }
+                                                src={pokemonDetails.sprites.front_default}
+                                            />
+                                            <Card.Text>
+                                                Forma Normal
+                                            </Card.Text>
+                                        </Col>
+                                        <Col>
+                                            <Card.Img
+                                                style={
+                                                    { width: "15rem" }
+                                                }
+                                                src={pokemonDetails.sprites.front_shiny}
+                                            />
+                                            <Card.Text>
+                                                Forma Shiny
+                                            </Card.Text>
+                                        </Col>
+                                    </Row>
+                                    <Row
+                                        className="mt-4"
+                                    >
+                                        <Col
+                                            xs={12}
+                                            sm={12}
+                                            md={12}
+                                            lg={12}
+                                            xl={12}
+                                        >
+                                            <div
+                                                className="px-4 py-1 mb-3 rounded"
+                                                style={
+                                                    { border: "1px #000 solid" }
+                                                }
+                                            >
+                                                Habilidades
+                                            </div>
+                                        </Col>
+                                    </Row>
+                                    <Row
+                                        className="text center"
+                                    >
+                                        {pokemonDetails.abilities.map(a => (
+                                            <Col
+                                                key={a.ability.name}
+                                                xs={6}
+                                                sm={6}
+                                                md={6}
+                                                lg={6}
+                                                xl={6}
+                                            >
+                                                <div
+                                                    className="rounded px-4 py-1"
+                                                >
+                                                    - {a.ability.name.toUpperCase()}
+                                                </div>
+                                            </Col>
+                                        ))}
+                                    </Row>
+                                </Card.Text>
+                            </Card.Body>
                         </Card>
                     </Col>
                 </Row>
