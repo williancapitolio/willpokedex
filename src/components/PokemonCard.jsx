6ef6f340d35acbad70345b1eedac1712a7172163
Willian <williansilvap25@gmail.com>
2023-02-09 22:31:46 -0300

    pokemon card

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,40 @@
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 export const PokemonCard = ({ pokemon }) => {
     return (
-        <div>
-            {pokemon.name}
-        </div>
+        <>
+            <Card
+                className="my-3 p-3 rounded text-center shadow mb-5 bg-white"
+            >
+                <Link
+                    to={`/pokemon/${pokemon.id}`}
+                >
+                    <Card.Img
+                        style={
+                            { width: "8rem" }
+                        }
+                        src={pokemon.sprites.front_default}
+                        variant="top"
+                    />
+                </Link>
+                <Card.Body
+                    className={`${pokemon.types[0].type.name} rounded text-white`}
+                >
+                    <Link 
+                        to={`/pokemon/${pokemon.id}`}
+                        className="link-name"
+                    >
+                        <Card.Title
+                            as="div"
+                        >
+                            <strong>
+                                #{pokemon.id} - {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+                            </strong>
+                        </Card.Title>
+                    </Link>
+                </Card.Body>
+            </Card>
+        </>
     );
 };
